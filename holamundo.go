870f679691262778c4820543cdf69f6ffeae688f
mauricio becerra <maurycyo@gmail.com>
2018-11-07 23:45:23 -0500

    primera subida

---
+++ holamundo.go
@@ -0,0 +1,43 @@
+package main
+import "fmt"
+func func main() {
+	fmt.Print("hola Super programador")
+}
+
+// CRTL P 		BUSCAR ARCHIVO LINEA DE COMANDOS (INSTALL PACKAGE) RECOMIENDA EMMET
+// CRTL SHIFT P BUSCAR ARCHIVO LINEA DE COMANDOS (INSTALL PACKAGE) RECOMIENDA EMMET
+// html:5 emmet crea archivo html basico
+// LOREM Y TAB  crea texto autocompletar
+// CRTL R BUSCAR FUNCION
+// COMAND KN ESCONDE ARCOL DIR
+// crtl shift buscar funciones
+// CTRL  r 		NOS da los ID
+// CTRL  N 		NUEVA pestaña
+// CTRL  shif N 	nueva ventana
+// CTRL + O -		AUMENTA O DISMINUYE TAMAÑO FONT
+// ALT 1,2,3 		NAVEGA ENTRR PESTAÑAS
+// ALT SHIFT 1,2,3 CREA 1 2 O 3 VENTANITAS COLUMNAS
+// CTRL  L 		SELECCIONA linea 
+// CTRL d 			SELLECIONA varias palabras al mismo tiempo
+// ctrl shift D    duplicar linea actual
+// CTRL click 		CREA varios cursores
+// CTRL SHIFT TAB	DESACE IDENTADO DE VARIAS 
+// CTRL shifr M 	selecciona codigo entre dos llaves
+// ctrl shift flecha der izs SELLECIONA atras adelante 
+// ctrl shift flecha arriba abajo mueve lineas 
+// CTRL shift ENT 	crea linea vacia y se queda el cursor aqui
+// CTRL shift K 	elimina la linea
+// CTRL kk 		ELIMINA lo que esta adelante
+// CTRL k RETROC 	elimina lo que esta detras
+// CTRL j 			QUITA los saltos de LINEA
+// CTRL KL			pasa a minusculas
+// CTRL KU			pasa a mayusculas
+// CTRL M			ver anidados inicio y fin
+// ctrl shift A 	seleciona toda una capa
+// ctrl } comentar varios pero no me funciona
+
+
+
+
+
+
